7ac5b4d0246343222226d44453d03d9f65580dd6
julioct99 <jcasallot01@gmail.com>
2024-07-29 14:15:13 +0200

    Cypress: Fix ts warnings from custom commands

---
+++ cypress/global.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    getBySel(dataTestAttribute: string, args?: unknown): Chainable<JQuery<HTMLElement>>
+    getBySelLike(
+      dataTestPrefixAttribute: string,
+      args?: unknown
+    ): Chainable<JQuery<HTMLElement>>
+  }
+}
