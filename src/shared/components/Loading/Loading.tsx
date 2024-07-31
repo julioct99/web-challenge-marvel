62b3eeefbc2cfd7b02615afe78c9b4d0dba707e0
julioct99 <jcasallot01@gmail.com>
2024-07-31 09:40:29 +0200

    Add response time indicator to the loading spinner

--- src/shared/components/Loading/Loading.tsx
+++ src/shared/components/Loading/Loading.tsx
@@ -8,6 +8,7 @@ const Loading: React.FunctionComponent<LoadingProps> = ({ loading = true }) => {
   return loading ? (
     <LoadingContainer>
       <LoadingSpinner />
+      <p>Please wait. Marvel's API response times are very slow.</p>
     </LoadingContainer>
   ) : null
 }
